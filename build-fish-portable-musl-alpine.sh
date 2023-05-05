8fdd768b9d43716a9432b403bf149329f8e627ec
Miguel Saldivar <miguel.saldivar@hpe.com>
2023-05-05 15:14:19 -0700

    change platform

--- build-fish-portable-musl-alpine.sh
+++ build-fish-portable-musl-alpine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -f fish-portable-musl-alpine.Dockerfile -t xxh/fish-portable-musl-alpine  #--no-cache --force-rm
+docker build --platform=arm64 . -f fish-portable-musl-alpine.Dockerfile -t xxh/fish-portable-musl-alpine  #--no-cache --force-rm
 docker run --rm -v `pwd`/result:/result xxh/fish-portable-musl-alpine
